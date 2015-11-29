8450517a6f77c6a6db811be44e6d15fdc4a1c9f2
Mattias Nordvall <mattias.nordvall@live.com>
2015-11-29 22:20:51 +0100

    Configured war packaging.

--- src/main/groovy/cloudsoa/service/springboot/Application.groovy
+++ src/main/groovy/cloudsoa/service/springboot/Application.groovy
@@ -2,9 +2,16 @@ package cloudsoa.service.springboot
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.context.web.SpringBootServletInitializer
 
 @SpringBootApplication
-class Application {
+class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     static void main(String[] args) {
         SpringApplication.run Application, args
