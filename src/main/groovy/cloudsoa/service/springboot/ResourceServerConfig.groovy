d8e86e51a79663024ad4470c5293a509f885a6e0
Mattias Nordvall <mattias.nordvall@live.com>
2015-12-13 19:51:44 +0100

    Upgrade to Spring Boot 1.3.

--- src/main/groovy/cloudsoa/service/springboot/ResourceServerConfig.groovy
+++ src/main/groovy/cloudsoa/service/springboot/ResourceServerConfig.groovy
@@ -1,12 +1,28 @@
 package cloudsoa.service.springboot
 
-import org.springframework.cloud.security.oauth2.resource.EnableOAuth2Resource
+//import org.springframework.cloud.security.oauth2.resource.EnableOAuth2Resource
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationProcessingFilter
 
 @Configuration
 @EnableResourceServer
-@EnableOAuth2Resource
-class ResourceServerConfig {
+//@EnableOAuth2Resource - deprecated in Spring Cloud 1.1, see https://github.com/spring-cloud/spring-cloud-security
+class ResourceServerConfig {//extends ResourceServerConfigurerAdapter  {
 
+    /*@Override
+    public void configure(HttpSecurity http) throws Exception {
+        //http.authorizeRequests().anyRequest().authenticated();
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        // @formatter:off
+        //resources.resourceId('http://mattiasnkuai.onmicrosoft.com/spring');
+        // @formatter:on
+    }*/
 }
